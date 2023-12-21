3ad29c1a4613fac7b6e3d0adaa67bbb4ebbd5449
Christian <ferchris82@hotmail.com>
2023-12-21 17:10:40 -0500

    Versión 1.1

--- src/org/chrisferdev/generics/EjemploGenericos.java
+++ src/org/chrisferdev/generics/EjemploGenericos.java
@@ -36,6 +36,11 @@ public class EjemploGenericos {
         imprimirClientes(clientesLista);
         imprimirClientes(clientePremiumList);
 
+        System.out.println("Máximo de 1, 9 y 4 es: " + maximo(1, 9, 4));
+        System.out.println("Máximo de 3.9, 11.6, 7.78 es: " + maximo(3.9, 11.6, 7.78));
+        System.out.println("Máximo de zanahoria, arándanos, manzana es: "
+                        + maximo("zanahoria", "arándano", "manzana"));
+
     }
     public static <T> List<T> fromArrayToList(T[] c){
         return Arrays.asList(c);
@@ -58,5 +63,16 @@ public class EjemploGenericos {
 
     public static void imprimirClientes(List<? extends Cliente> clientes){
         clientes.forEach(System.out::println);
+    }
+
+    public static <T extends Comparable<T>> T maximo(T a, T b, T c){
+        T max = a;
+        if(b.compareTo(max) > 0){
+            max = b;
+        }
+        if(c.compareTo(max) > 0){
+            max = c;
+        }
+        return max;
     }
 }
