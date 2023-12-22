97aac1b05f4bd6038a6ca9dd230cd0e790088a8c
Christian <ferchris82@hotmail.com>
2023-12-21 19:38:46 -0500

    Versión 1.1

--- src/org/chrisferdev/genericsclass/Camion.java
+++ src/org/chrisferdev/genericsclass/Camion.java
@@ -4,18 +4,18 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
-public class Camion implements Iterable {
+public class Camion<T> implements Iterable<T> {
 
-    private List objetos;
+    private List<T> objetos;
     private int max;
 
     public Camion(int max) {
         this.max = max;
-        this.objetos = new ArrayList();
+        this.objetos = new ArrayList<>();
     }
 
-    public void add(Object objeto){
-        if(this.objetos.size() >= max){
+    public void add(T objeto){
+        if(this.objetos.size() <= max){
             this.objetos.add(objeto);
         } else {
             throw new RuntimeException("no hay más espacio.");
@@ -23,7 +23,7 @@ public class Camion implements Iterable {
     }
 
     @Override
-    public Iterator iterator() {
+    public Iterator<T> iterator() {
         return this.objetos.iterator();
     }
 }
